3ac007511c8d039e290d6979896b8145e0750626
gavin <gallmond@gmail.com>
2021-12-08 09:09:12 +0000

    first commit

---
+++ src/Components/Main.svelte
@@ -0,0 +1,50 @@
+
+<script>
+
+  import TimeOption from './TimeOption.svelte'
+
+  let stages = [
+    0, // focus
+    1, // break
+    2  // long break
+  ]
+
+  let timeRemainingDisplay = '00:00'
+  let currentStage = stages[0]
+
+</script>
+
+<div>
+
+  <div>
+    <h1>🍅</h1>
+  </div>
+
+  <div>
+    <h2>
+      00:00
+    </h2>
+  </div>
+
+  <div>
+    <button>Start</button>
+  </div>
+
+  <div class="timeOptions">
+    {#each stages as stage}
+      <TimeOption active={currentStage === stage} />
+    {/each}
+    
+  </div>
+</div>
+
+<style>
+  .timeOptions{
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+  }
+  
+
+
+</style>
---
+++ src/Components/TimeOption.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let active = false;
+</script>
+
+<div class="timeOption">
+  {#if active}
+    <div>🔴</div>
+  {:else}
+    <div>⚪</div>
+  {/if}
+  <input type="text"/>
+</div>
+
+<style>
+  .timeOption{
+    flex:1
+  }
+  .timeOption > input{
+    margin-top: 5px;
+  }
+</style>
